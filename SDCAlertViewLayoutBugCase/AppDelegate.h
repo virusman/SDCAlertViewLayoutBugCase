3b56c73d17b559ce09661f8935f139dacdb83cd2
virusman <virusman@virusman.ru>
2015-03-26 14:17:23 +0300

    Initial Commit

---
+++ SDCAlertViewLayoutBugCase/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SDCAlertViewLayoutBugCase
+//
+//  Created by Victor Babenko on 26.03.15.
+//  Copyright (c) 2015 Victor Babenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
